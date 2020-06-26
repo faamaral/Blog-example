1e9f20b97b68ff1524f84bbc5b618642e193c679
faamaral <fabianoamaral445@gmail.com>
2020-06-26 20:57:21 -0300

    Criando o model Post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post (models.Model):
+
+    title = models.CharField(max)
+
+
 
 # Create your views here.
