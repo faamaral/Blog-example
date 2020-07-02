399d8a9ff7ac3f7a9ab66b97e419a31e4f388414
faamaral <fabianoamaral445@gmail.com>
2020-07-01 21:04:27 -0300

    Criando funcionalidade de adicionar um novo post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from . models import Post
 
 class BlogListView(ListView):
@@ -9,4 +10,9 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name = 'custom'
+    #context_object_name = 'custom'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_new.html'
+    fields = '__all__'
